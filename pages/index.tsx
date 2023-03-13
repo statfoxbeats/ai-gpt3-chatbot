58b7742d54b01d67229f81a2e449d911d3b79e4a
statfoxbeats <122543507+statfoxbeats@users.noreply.github.com>
2023-03-12 18:04:23 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,14 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+        <Text variant="h1">edit this</Text>
         <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
+you can edit this too
         </Text>
       </section>
 
       <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+        <Text variant="h2">You can edit this also too</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>
